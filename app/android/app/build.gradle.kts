2900b6524d22a9c9b2ea25e27b82bacbda7d1ebc
Crimson <johnny963214@gmail.com>
2026-02-08 19:37:33 +0800

    fix: Bundle ID 改回 com.example.parliament1812
    覆蓋舊 App，避免 iPhone 上出現兩個 App

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.parliament1812.parliament1812"
+    namespace = "com.example.parliament1812"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.parliament1812.parliament1812"
+        applicationId = "com.example.parliament1812"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24  // Required for Parliament 1812
